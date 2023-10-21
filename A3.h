a08745b7b8b361d8cf054f944ab869b17ac4cdde
Conj43 <118224244+Conj43@users.noreply.github.com>
2023-10-20 23:22:29 -0500

    Add files via upload

---
+++ A3.h
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<limits.h>
+#include<time.h>
+
+typedef struct buck{
+    float min;
+    float max;
+}Bucket;
+
+float findClusterExtreme(float array[], int n);   //n is size of array and returns smaller value of the widest-seperated pair in the array
+int interpolationSearch(float array[], int n, float k);
+Bucket * initBucketArray(int n);
+float * findMinMax(float array[], float * min, float * max, int n);
+void freeBucketArray(Bucket * p);
+void findWidestPair(Bucket p, Bucket * array, int currentIndex, float * minPair, float * maxPair, float * difference, int n, float max);
